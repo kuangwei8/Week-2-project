0c5d2b947ee99f240725d19cdaac454df74c7a4c
Wei Kuang <weibazai@gmail.com>
2019-06-07 16:45:30 -0700

    adding extra for show and hid function.

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,52 @@ $(document).ready(function(){
     event.preventDefault();
   });
 });
+
+
+
+// $(document).ready(function(){
+//   $("#questions").submit(function(event){
+//
+//   var personality=  $("input[type=radio][name=awsome]:checked" ).val();
+//
+//   var closet=
+//   $("input[type=radio][name=closet]:checked").val();
+//
+//   var term=  $("input[type=radio][name=term]:checked" ).val();
+//
+//   var friends=  $("input[type=radio][name=friends]:checked" ).val();
+//
+//   var pink=  $("input[type=radio][name=color]:checked" ).val();
+//
+//   if (personality === "yes" && closet === "yes" ){
+//       $("#python").show();
+//       $("#ruby").hide();
+//       $("#java").hide();
+//       $("#javascript").hide();
+//     };
+//
+//    if (friends === "yes" && pink ==="yes"){
+//      $("#java").show();
+//      $("#ruby").hide();
+//      $("#python").hide();
+//      $("#javascript").hide();
+//    };
+//
+//    if (closet === "no" && friends === "no"){
+//      $("#javascript").show();
+//      $("#ruby").hide();
+//      $("#java").hide();
+//      $("#python").hide();
+//    };
+//
+//    if else {
+//       $("#ruby").show();
+//       $("#python").hide();
+//       $("#java").hide();
+//       $("#javascript").hide();
+//     };
+//
+//
+//     event.preventDefault();
+//   });
+// });
