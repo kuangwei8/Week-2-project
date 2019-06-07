77d7dfd4d4161b0508a5ed567f968262d544b768
Wei Kuang <weibazai@gmail.com>
2019-06-07 14:44:06 -0700

    adding different htmls for different language

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
 $(document).ready(function(){
   $("#questions").submit(function(event){
 
-  var personaity=  $("input[type=radio][name=awsome]:checked" ).val();
+  var personality=  $("input[type=radio][name=awsome]:checked" ).val();
 
-  // var closet=
-  // $("input[type=radio][name=closet]:checked").val();
+  var closet=
+  $("input[type=radio][name=closet]:checked").val();
 
-  // var term=  $("input[type=radio][name=term]:checked" ).val();
+  var term=  $("input[type=radio][name=term]:checked" ).val();
+
+  var friends=  $("input[type=radio][name=friends]:checked" ).val();
+
+  var pink=  $("input[type=radio][name=color]:checked" ).val();
+
+
+
+
+  // if (personality === "yes" && closet === "yes" ) {
+  //  location.replace("python.html");}
   //
-  // var friends=  $("input[type=radio][name=friends]:checked" ).val();
-  //
-  // var pink=  $("input[type=radio][name=pink]:checked" ).val();
-
-
-  //
-  console.log(personality);
-  if (personality === "yes"){
-  alert("how are you");}
+  // if (friends === "yes" && pink ==="yes") {
+  //   location.replace("java.html");
+  // };
+  if (closet === "no" && friends === "no"){
+    location.replace("javascript.html");
+  } else {location.replace("ruby.html");}
 
 
 
     event.preventDefault();
-  // });
+  });
 });
