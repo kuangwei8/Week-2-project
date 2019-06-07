22d70dc8aa631f05858f2fce2b7def14a0c4e70d
Wei Kuang <weibazai@gmail.com>
2019-06-07 14:00:13 -0700

    add variables in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
   $("#questions").submit(function(event){
-    var personality = $
+
+  var personaity=  $("input[type=radio][name=awsome]:checked" ).val();
+
+  // var closet=
+  // $("input[type=radio][name=closet]:checked").val();
+
+  // var term=  $("input[type=radio][name=term]:checked" ).val();
+  //
+  // var friends=  $("input[type=radio][name=friends]:checked" ).val();
+  //
+  // var pink=  $("input[type=radio][name=pink]:checked" ).val();
+
+
+  //
+  console.log(personality);
+  if (personality === "yes"){
+  alert("how are you");}
 
 
 
     event.preventDefault();
-  });
+  // });
 });
