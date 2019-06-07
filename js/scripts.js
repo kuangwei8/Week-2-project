a5b1efa3c39252cf065817caee9fcfa348e81b40
Wei Kuang <weibazai@gmail.com>
2019-06-07 11:23:41 -0700

    made all the radio buttons work.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@
 //
 //   });
 // });
+var json = {
+    "questions": [
+        {
+            "type": "radiogroup",
+            "name": "one",
+            "title": "Radiogroup question (green)",
+            "choices": ["Yes", "No"]
+        }, {
+            "type": "checkbox",
+            "name": "two",
+            "title": "Checkbox question (orange)",
+            "choices": ["One", "Two"]
+        }, {
+            "type": "radiogroup",
+            "name": "three",
+            "title": "Required question (red title)",
+            "isRequired": true,
+            "choices": ["Yes", "No"]
+        }
+
+    ]
+}
