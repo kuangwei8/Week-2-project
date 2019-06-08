1572e231301039c42209ed78ac8cc1b734487e41
Wei Kuang <weibazai@gmail.com>
2019-06-07 23:07:50 -0700

    Final edit for week 2 project.

--- js/scripts.js
+++ js/scripts.js
@@ -29,14 +29,13 @@ $(document).ready(function(){
 });
 
 
-
+//
 // $(document).ready(function(){
 //   $("#questions").submit(function(event){
 //
 //   var personality=  $("input[type=radio][name=awsome]:checked" ).val();
 //
-//   var closet=
-//   $("input[type=radio][name=closet]:checked").val();
+//   var closet= $("input[type=radio][name=closet]:checked").val();
 //
 //   var term=  $("input[type=radio][name=term]:checked" ).val();
 //
@@ -44,35 +43,31 @@ $(document).ready(function(){
 //
 //   var pink=  $("input[type=radio][name=color]:checked" ).val();
 //
-//   if (personality === "yes" && closet === "yes" ){
-//       $("#python").show();
-//       $("#ruby").hide();
-//       $("#java").hide();
-//       $("#javascript").hide();
-//     };
+//   if (personality == null || closet == null || term == null || friends == null || pink == null ){
+//   alert("You need to answer all the questions!");
+//   }else if (personality === "yes" && closet === "yes" ){
+//     $("#python").show();
+//     $("#ruby").hide();
+//     $("#java").hide();
+//     $("#javascript").hide();
+//   }else if (friends === "yes" && pink ==="yes"){
+//    $("#java").show();
+//    $("#ruby").hide();
+//    $("#python").hide();
+//    $("#javascript").hide();
+//  }else if (closet === "no" && friends === "no"){
+//    $("#javascript").show();
+//    $("#ruby").hide();
+//    $("#java").hide();
+//    $("#python").hide();
+//  }else {
+//     $("#ruby").show();
+//     $("#python").hide();
+//     $("#java").hide();
+//     $("#javascript").hide()
+//   };
 //
-//    if (friends === "yes" && pink ==="yes"){
-//      $("#java").show();
-//      $("#ruby").hide();
-//      $("#python").hide();
-//      $("#javascript").hide();
-//    };
-//
-//    if (closet === "no" && friends === "no"){
-//      $("#javascript").show();
-//      $("#ruby").hide();
-//      $("#java").hide();
-//      $("#python").hide();
-//    };
-//
-//    if else {
-//       $("#ruby").show();
-//       $("#python").hide();
-//       $("#java").hide();
-//       $("#javascript").hide();
-//     };
-//
-//
+// 
 //     event.preventDefault();
 //   });
 // });
